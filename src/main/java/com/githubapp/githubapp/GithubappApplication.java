6f4393af525284c8ca700ab8bb24baf5c5f8abd0
Amit Kumar <amit@Amits-Laptop.local>
2024-11-06 19:41:59 +0100

    first commit

---
+++ src/main/java/com/githubapp/githubapp/GithubappApplication.java
@@ -0,0 +1,13 @@
+package com.githubapp.githubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubappApplication.class, args);
+	}
+
+}
